5bc8bf4b27ec965f989288fc064d897448683621
Yuxuan Xie <teststaybaka@gmail.com>
2021-07-19 06:30:17 +0000

    Make LocalSessionStorage a singleton to be dependant on.

---
+++ frontend/web/main/local_session_storage.ts
@@ -0,0 +1,3 @@
+import { LocalSessionStorage } from "@selfage/service_client/local_session_storage";
+
+export let LOCAL_SESSION_STORAGE = new LocalSessionStorage();
--- frontend/web/main/service_client.ts
+++ frontend/web/main/service_client.ts
@@ -1,7 +1,7 @@
+import { LOCAL_SESSION_STORAGE } from "./local_session_storage";
 import { ServiceClient } from "@selfage/service_client";
-import { LocalSessionStorage } from "@selfage/service_client/local_session_storage";
 
 export let SERVICE_CLIENT = new ServiceClient(
-  new LocalSessionStorage(),
+  LOCAL_SESSION_STORAGE,
   window.fetch
 );
