037fb999849ca1a84381ebf487932b730ae3b856
Yuxuan Xie <teststaybaka@gmail.com>
2021-08-14 18:57:37 +0000

    Migrate SwitchCheckboxComponent.

---
+++ frontend/chrome_extension/content_script/display_settings_tab/common.ts
@@ -0,0 +1,7 @@
+import { ColorScheme } from "../../../color_scheme";
+
+export let ENTRY_MAGIN_TOP = `margin-top: .5rem;`;
+export let LABEL_STYLE =
+  `flex-basis: 0; font-size: 1.4rem; font-family: initial !important; ` +
+  `text-overflow: ellipsis; color: ${ColorScheme.getContent()};`;
+export let INPUT_WIDTH = "width: 4rem;";
---
+++ frontend/chrome_extension/content_script/display_settings_tab/switch_checkbox_component.ts
@@ -0,0 +1,118 @@
+import EventEmitter = require("events");
+import { ColorScheme } from "../../../color_scheme";
+import { ENTRY_MAGIN_TOP, INPUT_WIDTH, LABEL_STYLE } from "./common";
+import { E } from "@selfage/element/factory";
+import { Ref } from "@selfage/ref";
+
+export interface SwitchCheckboxComponent {
+  on(event: "change", listener: (value: boolean) => void): this;
+}
+
+export class SwitchCheckboxComponent extends EventEmitter {
+  private static RADIUS = ".9rem";
+  private static TRANSITION_DURATION = ".3s";
+
+  public constructor(
+    public body: HTMLDivElement,
+    private switchBarLeft: HTMLDivElement,
+    private switchBarRight: HTMLDivElement,
+    private switchCircle: HTMLDivElement,
+    private defaultValue: boolean,
+    private value: boolean
+  ) {
+    super();
+  }
+
+  public static create(
+    label: string,
+    defaultValue: boolean,
+    value: boolean
+  ): SwitchCheckboxComponent {
+    return new SwitchCheckboxComponent(
+      ...SwitchCheckboxComponent.createView(label),
+      defaultValue,
+      value
+    ).init();
+  }
+
+  public static createView(label: string) {
+    let switchBarLeft: Ref<HTMLDivElement> = {};
+    let switchBarRight: Ref<HTMLDivElement> = {};
+    let switchCircle: Ref<HTMLDivElement> = {};
+    let body = E.div(
+      `class="switch-checkbox-container" style="display: flex; ` +
+        `flex-flow: row nowrap; justify-content: space-between; ` +
+        `align-items: center; ${ENTRY_MAGIN_TOP} cursor: pointer;"`,
+      E.div(
+        `class="switch-checkbox-label", style="${LABEL_STYLE}" ` +
+          `title="${label}"`,
+        E.text(label)
+      ),
+      E.div(
+        `class="switch-checkbox-wrapper" style="position: relative; ` +
+          `${INPUT_WIDTH}"`,
+        E.divRef(
+          switchBarLeft,
+          `class="switch-checkbox-bar-left" style="display: inline-block; ` +
+            `height: 1.8rem; ` +
+            `border-radius: ${SwitchCheckboxComponent.RADIUS} 0 0 ${SwitchCheckboxComponent.RADIUS};` +
+            `background-color: ${ColorScheme.getSwitchOffBackground()}; ` +
+            `transition: width ${SwitchCheckboxComponent.TRANSITION_DURATION};`
+        ),
+        E.divRef(
+          switchBarRight,
+          `class="switch-checkbox-bar-right" style="display: inline-block; ` +
+            `height: 1.8rem; ` +
+            `border-radius: 0 ${SwitchCheckboxComponent.RADIUS} ${SwitchCheckboxComponent.RADIUS} 0; ` +
+            `background-color: ${ColorScheme.getSwitchOnBackground()}; ` +
+            `transition: width ${SwitchCheckboxComponent.TRANSITION_DURATION};`
+        ),
+        E.divRef(
+          switchCircle,
+          `class="switch-checkbox-circle" style="position: absolute; ` +
+            `height: 1.8rem; width: 1.8rem; ` +
+            `left: -${SwitchCheckboxComponent.RADIUS}; ` +
+            `background-color: ${ColorScheme.getBackground()}; ` +
+            `border: .1rem solid ${ColorScheme.getInputBorder()}; ` +
+            `border-radius: 50%; ` +
+            `transition: margin-left ${SwitchCheckboxComponent.TRANSITION_DURATION};"`
+        )
+      )
+    );
+    return [
+      body,
+      switchBarLeft.val,
+      switchBarRight.val,
+      switchCircle.val,
+    ] as const;
+  }
+
+  public init(): this {
+    this.setValue(this.value);
+    this.body.addEventListener("click", () => this.toggleSwitch());
+    return this;
+  }
+
+  private setValue(value: boolean): void {
+    this.value = value;
+    if (this.value) {
+      this.switchBarLeft.style.width = SwitchCheckboxComponent.RADIUS;
+      this.switchBarRight.style.width = `calc(100% - ${SwitchCheckboxComponent.RADIUS})`;
+      this.switchCircle.style.marginLeft = SwitchCheckboxComponent.RADIUS;
+    } else {
+      this.switchBarLeft.style.width = `calc(100% - ${SwitchCheckboxComponent.RADIUS})`;
+      this.switchBarRight.style.width = SwitchCheckboxComponent.RADIUS;
+      this.switchCircle.style.marginLeft = `calc(100% - ${SwitchCheckboxComponent.RADIUS})`;
+    }
+  }
+
+  private toggleSwitch(): void {
+    this.setValue(!this.value);
+    this.emit("change", this.value);
+  }
+
+  public reset(): boolean {
+    this.setValue(this.defaultValue);
+    return this.value;
+  }
+}
