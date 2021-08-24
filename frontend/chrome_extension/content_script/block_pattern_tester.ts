458c616d083e2f34a4bb6217b74f573ac45e4754
Yuxuan Xie <teststaybaka@gmail.com>
2021-08-24 06:14:35 +0000

    Migrate BlockPatternTester

---
+++ frontend/chrome_extension/content_script/block_pattern_tester.ts
@@ -0,0 +1,58 @@
+import { ChatEntry } from "../../../interface/chat_entry";
+import { BlockKind, BlockSettings } from "../../../interface/player_settings";
+
+export interface ContentExtractor {
+  extract: (content: string) => string;
+}
+
+class IdentityExtractor implements ContentExtractor {
+  public extract(content: string): string {
+    return content;
+  }
+}
+
+class HtmlExtractor implements ContentExtractor {
+  private static TAG_REPLACER = /<.*?>/g;
+
+  public extract(content: string): string {
+    return content.replace(HtmlExtractor.TAG_REPLACER, "");
+  }
+}
+
+export class BlockPatternTester {
+  public constructor(
+    private blockSettings: BlockSettings,
+    private contentExtractor: ContentExtractor
+  ) {}
+
+  public static createIdentity(
+    blockSettings: BlockSettings
+  ): BlockPatternTester {
+    return new BlockPatternTester(blockSettings, new IdentityExtractor());
+  }
+
+  public static createHtml(blockSettings: BlockSettings): BlockPatternTester {
+    return new BlockPatternTester(blockSettings, new HtmlExtractor());
+  }
+
+  public test(chatEntry: ChatEntry): boolean {
+    for (let blockPattern of this.blockSettings.blockPatterns) {
+      let content = this.contentExtractor.extract(chatEntry.content);
+      switch (blockPattern.kind) {
+        case BlockKind.Keyword:
+          if (content.indexOf(blockPattern.content) !== -1) {
+            return true;
+          }
+          break;
+        case BlockKind.RegExp:
+          let regExp = new RegExp(blockPattern.content);
+          if (regExp.test(content)) {
+            return true;
+          }
+          break;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ frontend/chrome_extension/content_script/block_pattern_tester_test.ts
@@ -0,0 +1,82 @@
+import { BlockPatternTester, ContentExtractor } from "./block_pattern_tester";
+import { assertThat, eq } from "@selfage/test_matcher";
+import { NODE_TEST_RUNNER } from "@selfage/test_runner";
+
+NODE_TEST_RUNNER.run({
+  name: "BlockPatternTesterTest",
+  cases: [
+    {
+      name: "BlockKeywords",
+      execute: () => {
+        // Prepare
+        let contentExtractor = new (class implements ContentExtractor {
+          public extract(content: string) {
+            assertThat(content, eq("test content"), "content");
+            return "Contains a key!";
+          }
+        })();
+        let blockPatternTester = new BlockPatternTester(
+          {
+            blockPatterns: [{ kind: 1, content: "key" }],
+          },
+          contentExtractor
+        );
+
+        // Execute
+        let blocked = blockPatternTester.test({ content: "test content" });
+
+        // Verify
+        assertThat(blocked, eq(true), "blocked");
+      },
+    },
+    {
+      name: "BlockRegex",
+      execute: () => {
+        // Prepare
+        let contentExtractor = new (class implements ContentExtractor {
+          public extract(content: string) {
+            return "Has a regex.";
+          }
+        })();
+        let blockPatternTester = new BlockPatternTester(
+          {
+            blockPatterns: [{ kind: 3, content: "^Has.*?reg.*?$" }],
+          },
+          contentExtractor
+        );
+
+        // Execute
+        let blocked = blockPatternTester.test({});
+
+        // Verify
+        assertThat(blocked, eq(true), "blocked");
+      },
+    },
+    {
+      name: "NotBlocked",
+      execute: () => {
+        // Prepare
+        let contentExtractor = new (class implements ContentExtractor {
+          public extract(content: string) {
+            return "OK.";
+          }
+        })();
+        let blockPatternTester = new BlockPatternTester(
+          {
+            blockPatterns: [
+              { kind: 1, content: "key" },
+              { kind: 3, content: "^Has.*?reg.*?$" },
+            ],
+          },
+          contentExtractor
+        );
+
+        // Execute
+        let blocked = blockPatternTester.test({});
+
+        // Verify
+        assertThat(blocked, eq(false), "not blocked");
+      },
+    },
+  ],
+});
