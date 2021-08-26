df3fff66b4b3ec40848b900b3855147fc275621a
Yuxuan Xie <teststaybaka@gmail.com>
2021-08-26 04:16:31 +0000

    Migrate ChromeRuntime

---
+++ frontend/chrome_extension/content_script/chrome_runtime.ts
@@ -0,0 +1,18 @@
+export class ChromeRuntime {
+  public sendMessage(request: any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      chrome.runtime.sendMessage(
+        chrome.runtime.id,
+        request,
+        {},
+        (response: any) => {
+          if (!response) {
+            reject(chrome.runtime.lastError);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+    });
+  }
+}
