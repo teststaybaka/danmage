4ce3fa7d814ad600a88a8116ac9cf369f5b6c815
Yuxuan Xie <teststaybaka@gmail.com>
2021-08-29 22:32:47 +0000

    Add factory method to GlobalDocuments.

--- frontend/chrome_extension/content_script/global_documents.ts
+++ frontend/chrome_extension/content_script/global_documents.ts
@@ -1,6 +1,10 @@
 export class GlobalDocuments {
   public constructor(private documents: Document[]) {}
 
+  public static create(documents: Document[]): GlobalDocuments {
+    return new GlobalDocuments(documents);
+  }
+
   public hideWhenMousedown(carvedElement: HTMLElement, hide: () => void): void {
     for (let document of this.documents) {
       document.addEventListener("mousedown", (event) => {
