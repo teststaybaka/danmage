ee78b6fd59224f3f9c1adf32607dcda9b5923326
Yuxuan Xie <teststaybaka@gmail.com>
2021-11-01 01:00:16 +0000

    Set font-family to input.

--- frontend/web/main/common_style.ts
+++ frontend/web/main/common_style.ts
@@ -5,5 +5,6 @@ export let LABEL_STYLE =
   `font-size: 1.4rem; width: 10rem; ` + `color: ${ColorScheme.getContent()};`;
 export let INPUT_STYLE =
   `padding: 0; margin: 0; outline: none; border: 0; ` +
-  `background-color: initial; flex: 3; font-size: 1.4rem; ` +
+  `background-color: initial; flex: 3; font-size: 1.4rem; `+
+  `font-family: initial; `+
   `border-bottom: .1rem solid ${ColorScheme.getInputBorder()};`;
